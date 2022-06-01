c531b8fee4b7d3425c40f37b12877beca45fa3d1
Erekle Tvinadze <Erekle_Tvinadze@epam.com>
2022-06-01 19:23:25 +0400

    slight change

--- reverse_string.go
+++ reverse_string.go
@@ -1,9 +1,9 @@
 package reverse_string
 
 func ReverseString(input string) (output string) {
-	runes := []rune(input)
-	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
-		runes[i], runes[j] = runes[j], runes[i]
+	runes1 := []rune(input)
+	for i, j := 0, len(runes1)-1; i < j; i, j = i+1, j-1 {
+		runes1[i], runes1[j] = runes1[j], runes1[i]
 	}
-	return string(runes)
+	return string(runes1)
 }
